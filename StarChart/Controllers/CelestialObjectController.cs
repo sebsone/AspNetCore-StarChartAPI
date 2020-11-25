6785a2c7f590728fa2ed0b39f629f5e1e466ce60
Sebastian Vatland Sonesen <sebastian.sonesen@bouvet.no>
2020-11-25 13:09:53 +0100

    Completed module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -18,5 +19,28 @@ namespace StarChart.Controllers
             _context = context;
         }
 
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int Id)
+        {
+            CelestialObject obj = _context.CelestialObjects.Where(o => o.Id == Id).FirstOrDefault();
+            if (obj == null) return NotFound();
+            //obj.Satellites.Add(_context.CelestialObjects.Where(o => o.OrbitedObjectId == Id));
+            return Ok(obj);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            CelestialObject obj = _context.CelestialObjects.Where(o => o.Name == name).FirstOrDefault();
+            if (obj == null) return NotFound();
+            return Ok(obj);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<CelestialObject> objs = _context.CelestialObjects.ToList();
+            return Ok(objs);
+        }
     }
 }
