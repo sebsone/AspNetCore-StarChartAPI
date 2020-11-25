cb2c24f70621e8fe7322bb3c9e3166242469dc65
Sebastian Vatland Sonesen <sebastian.sonesen@bouvet.no>
2020-11-25 11:00:57 +0100

    Completed module

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+
+        public DbSet<CelestialObject> CelestialObjects;
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+            
         }
     }
 }
---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int id;
+
+        [Required]
+        public string Name;
+
+        public int? OrbitedObjectId;
+
+        [NotMapped]
+        public List<CelestialObject> Satellites;
+
+        public TimeSpan OrbitalPeriod;
+    }
+}
